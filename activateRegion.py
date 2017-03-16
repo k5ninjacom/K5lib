2fab73d29384da34469eea915e003dcfe720f84c
mika <mika.hahtokari@codershouse.com>
2017-03-16 18:47:24 +0200

    fitst version of activateregion.py

--- activateRegion.py
+++ activateRegion.py
@@ -1,2 +1,14 @@
+from os import environ as env
 import k5lib
 
+
+username = env['OS_USERNAME']
+password = env['OS_PASSWORD']
+domain = env['OS_USER_DOMAIN_NAME']
+
+globalToken = k5lib.authenticate.get_global_token(username, password, domain)
+domainId = k5lib.authenticate.get_domain_id(user, password, domain)
+regionId = 'fi-1'
+
+response = k5lib.contract.activate_region(globalToken, domainId, regionId)
+print (response)
