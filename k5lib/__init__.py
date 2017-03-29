64bcaa3a69d986a3dc35b85c04ee609338c99536
mika <mika.hahtokari@codershouse.com>
2017-03-29 18:56:01 +0300

    rearrange init entries

--- k5lib/__init__.py
+++ k5lib/__init__.py
@@ -1,8 +1,8 @@
-from .authenticate import get_region_token
-from .authenticate import get_region_info
-from .authenticate import get_project_token
 from .authenticate import get_global_token
+from .authenticate import get_region_token
+from .authenticate import get_project_token
 from .authenticate import get_domain_id
+from .authenticate import get_region_info
 from .authenticate import get_defaultproject_id
 
 from .contract import list_regions
