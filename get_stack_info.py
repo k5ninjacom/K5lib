98157f14daeaf3f6225093f144c0d1bf1bf5bbe8
mika <mika.hahtokari@codershouse.com>
2017-04-10 18:02:30 +0300

    get_stack info saves now private.key

--- get_stack_info.py
+++ get_stack_info.py
@@ -16,11 +16,31 @@ projectToken = k5lib.get_project_token(username, password, domain, projectName, 
 projectId = k5lib.get_project_id(username, password, domain, projectName, region)
 
 stackName = 'devops_tools'
-stackId = '8686e252-334c-4e56-b424-573f64f4c567'
+stackId = '144955db-b1b5-490c-87ea-a52fc8eca9fa'
 
 stackInfo = k5lib.get_stack_info(projectToken, projectId, region, stackName, stackId)
 
 logging.info(json.dumps(stackInfo, indent=4))
 print(json.dumps(stackInfo, indent = 4))
-#print(stackInfo)
+
+stackOutput = stackInfo["stack"]["outputs"]
+
+file = open('private.key', 'w')
+
+outputList = []
+outputDict = stackInfo["stack"]["outputs"]
+
+counter = 0
+for i in outputDict:
+    if str(i['output_key']) == 'app_private_key':
+        file.write(str(i['output_value']))
+        outputList.append(str(i['output_value']))
+        counter += 1
+
+print(outputList)
+
+
+
+
+
 
