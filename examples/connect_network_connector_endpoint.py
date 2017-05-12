54ca56549c33b5be8717f427367f82e176f9446b
mika <mika.hahtokari@codershouse.com>
2017-05-12 13:36:52 +0300

    connect_network_connector_endpoint try 16

--- examples/connect_network_connector_endpoint.py
+++ examples/connect_network_connector_endpoint.py
@@ -35,7 +35,7 @@ for i in outputDict:
 print(outputList[0])
 networkconnectorId = outputList[0]
 
-connectorEndpoint = k5lib.create_network_connector_endpoint(projectToken, projectId, region, az, endpointName01)
+connectorEndpoint = k5lib.create_network_connector_endpoint(projectToken, projectId, region, az, endpointName01, networkconnectorId)
 print(connectorEndpoint)
 
 # connectstatus = k5lib.connect_network_connector_endpoint(projectToken, region, endpointId, portId)
