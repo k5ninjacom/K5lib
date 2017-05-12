784ce79cd0b41d7f1d7d1ad1814e4f8b276e8474
mika <mika.hahtokari@codershouse.com>
2017-05-12 11:17:02 +0300

    nicer output for list_network_connections.py example

--- examples/list_network_connections.py
+++ examples/list_network_connections.py
@@ -15,4 +15,4 @@ region = env['OS_REGION_NAME']
 
 projectToken = k5lib.get_project_token(username, password, domain, projectName, region)
 network_connections = k5lib.list_network_connectors(projectToken, region)
-print(network_connections)
+print(json.dumps(network_connections, indent=2))
