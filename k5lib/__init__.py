a0b92dbe8fad42ba0f3fb49bf43ec360c55d8e10
mika <mika.hahtokari@codershouse.com>
2017-05-12 17:37:52 +0300

    delete_port example

--- k5lib/__init__.py
+++ k5lib/__init__.py
@@ -36,6 +36,7 @@ from .network import connect_network_connector_endpoint
 from .network import delete_network_connector_endpoint
 from .network import create_port_on_network
 from .network import list_ports
+from .network import delete_port
 from .network import get_port_id
 from .utils import create_logfile
 from .utils import gen_passwd
