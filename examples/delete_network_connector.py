6cb0af81ff1a82f2e1b25b6164fb875379d9a4bf
mika <mika.hahtokari@codershouse.com>
2017-05-12 16:49:10 +0300

    added delete_network_connector function into network library fix 2

--- examples/delete_network_connector.py
+++ examples/delete_network_connector.py
@@ -17,5 +17,9 @@ az = 'fi-1a'
 securityGroupId = '91630e71-2555-4dcb-a720-0dd3c643f478'
 networkId ='6809bf51-a224-4f16-a77e-754c3033b1b6'
 
+projectToken = k5lib.get_project_token(username, password, domain, projectName, region)
+projectId = k5lib.get_project_id(username, password, domain, projectName, region)
+
+
 connectors = k5lib.list_network_connectors(projectToken, region)
 print((json.dumps(connectors, indent=2)))
