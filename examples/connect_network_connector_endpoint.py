9804e058b3ad3c5a97df0912f04382c12de1c734
mika <mika.hahtokari@codershouse.com>
2017-05-12 15:31:35 +0300

    connect_network_connector_endpoint try 24

--- examples/connect_network_connector_endpoint.py
+++ examples/connect_network_connector_endpoint.py
@@ -13,42 +13,55 @@ domain = env['OS_USER_DOMAIN_NAME']
 projectName = env['OS_PROJECT_NAME']
 region = env['OS_REGION_NAME']
 az = 'fi-1a'
-endpointName01 = 'mhaNetworkConnector-ep01'
-connectorName = 'mhaNetworkConnector01'
-portName = 'mha-port02'
 
 securityGroupId = '91630e71-2555-4dcb-a720-0dd3c643f478'
 networkId ='6809bf51-a224-4f16-a77e-754c3033b1b6'
 
+endpointName01 = 'mhaNetworkConnector-ep03'
+connectorName = 'mhaNetworkConnector03'
+portName = 'mha-port03'
+
 projectToken = k5lib.get_project_token(username, password, domain, projectName, region)
 projectId = k5lib.get_project_id(username, password, domain, projectName, region)
 
+print('networkconnector')
+# Create new connector
 newConnector = k5lib.create_network_connector(projectToken, projectId, region, connectorName)
 print(newConnector)
 
-# networkConnectorList = k5lib.list_network_connectors(projectToken, region)
-# print((json.dumps(networkConnectorList, indent=2)))
-
+# Get ID for new connector to verify ID:s are same
 networkconnectorId = k5lib.get_network_connector_id(projectToken, region, connectorName)
 print(networkconnectorId)
 
+# Get list of network connectors
+networkConnectorList = k5lib.list_network_connectors(projectToken, region)
+print((json.dumps(networkConnectorList, indent=2)))
 
-# connectorEndpoint = k5lib.create_network_connector_endpoint(projectToken, projectId, region, az, endpointName01, networkconnectorId)
 
-# connectorEndpointlist = k5lib.list_network_connector_endpoints(projectToken, region)
-# print(json.dumps(connectorEndpointlist, indent=2))
+print('Network connector endpoint')
 
+# Create new connector endpoint
+newconnectorEndpoint = k5lib.create_network_connector_endpoint(projectToken, projectId, region, az, endpointName01, networkconnectorId)
+print(newconnectorEndpoint)
+
+# Get ID for new connector endpoint to verify ID:s are same
 connectorEnpointId = k5lib.get_network_connector_endpoint_id(projectToken, region, endpointName01)
 print(connectorEnpointId)
 
-# portId = k5lib.create_port_on_network(projectToken, region, az, portName, securityGroupId, networkId)
-# print(portId)
+# Get list of network connector endpoints
+connectorEndpointlist = k5lib.list_network_connector_endpoints(projectToken, region)
+print(json.dumps(connectorEndpointlist, indent=2))
 
-portList = ''
+print ('port')
+
+# Create a new network port
+newPort = k5lib.create_port_on_network(projectToken, region, az, portName, securityGroupId, networkId)
+print(newPort)
+
+# Get ID for new port to verify ID:s are the same
+portList = 'portlist'
 print(portList)
 
-portId = ''
-print(portId)
 
 # connect = k5lib.connect_network_connector_endpoint(projectToken, region, connectorEnpointId, portId)
 # print(json.dumps(connect, indent=2))
