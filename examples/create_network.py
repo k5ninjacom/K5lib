cfe2291083861b8420b043a467bbf5738aced2b6
mika <mika.hahtokari@codershouse.com>
2017-05-17 12:55:20 +0300

    create_network example fixes 1

--- examples/create_network.py
+++ examples/create_network.py
@@ -7,6 +7,7 @@ username = env['OS_USERNAME']
 password = env['OS_PASSWORD']
 domain = env['OS_USER_DOMAIN_NAME']
 region = env['OS_REGION_NAME']
+projectName = envenv['OS_PROJECT_NAME']
 networkName = 'foobar-network'
 az = 'fi-1a'
 
@@ -14,6 +15,6 @@ az = 'fi-1a'
 k5lib.create_logfile('create_network.log')
 
 
-regionToken = k5lib.get_region_token(username, password, domain, region)
+projectToken = k5lib.get_project_token(username, password, domain, projectname, region)
 network = k5lib.create_network(projectToken, region, az, networkName)
 print(network)
