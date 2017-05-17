7d197b00bbca51cab77e615509e07248df91a870
mika <mika.hahtokari@codershouse.com>
2017-05-17 16:55:53 +0300

    added optional variable handling in create_subnet function fix 15

--- k5lib/network.py
+++ k5lib/network.py
@@ -872,7 +872,8 @@ def _rest_create_subnet(project_token, region,  network_id, cidr, subnet_name, v
     #        value = ''
 
     for key, value in recursive_items(configData):
-        print(key, value)
+        if value is None:
+            print(key, value)
 
 #    print(dict_iter(configData))
 #    for key, value in configData[subnet].items():
