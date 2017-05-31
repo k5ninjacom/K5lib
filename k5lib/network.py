9fe45e76fdd626e2cefcd46b3181e4b12ce7eeae
mika <mika.hahtokari@codershouse.com>
2017-05-31 17:01:18 +0300

    Added delete_subnet feature

--- k5lib/network.py
+++ k5lib/network.py
@@ -925,6 +925,40 @@ def create_subnet(project_token, region, network_id, cidr, subnet_name='subnet',
     else:
         return request.json()['subnet']['id']
 
+def _rest_delete_subnet(project_token, region):
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json',
+               'X-Auth-Token': project_token}
+
+    configData = {'key1': {
+                     'key2': [
+                          {
+                              'key3': 'value3'
+                          }
+                     ]
+                 }
+    }
+
+    url = url = 'https://foobar.' + region + '.cloud.global.fujitsu.com/v2.0/ports'
+
+    try:
+        request = requests.post(url, json=configData, headers=headers)
+        request.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        # Whoops it wasn't a 200
+        log.error(json.dumps(configData, indent=4))
+        return 'Error: ' + str(e)
+    else:
+        return request
+
+
+def delete_subnet(project_token, region):
+    request = _rest_stub(project_token, region)
+    if 'Error' in str(request):
+        return str(request)
+    else:
+        return request.json()
+
 
 def _rest_create_security_group(project_token, region, name, description):
     headers = {'Content-Type': 'application/json',
