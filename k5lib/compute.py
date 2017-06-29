5b9a4880eec7b2efdb979712d75da2c5a394915f
mika <mika.hahtokari@fi.fujitsu.com>
2017-06-29 18:07:54 +0300

    fixed url in to get_server_password

--- k5lib/compute.py
+++ k5lib/compute.py
@@ -102,7 +102,7 @@ def _rest_get_server_password(project_token, region, project_id, server_id):
 
 
 
-    url = 'https://compute.' + region + '.cloud.global.fujitsu.com/v2' + project_id + '/servers/' + server_id + '/os-server-password'
+    url = 'https://compute.' + region + '.cloud.global.fujitsu.com/v2/' + project_id + '/servers/' + server_id + '/os-server-password'
 
     try:
         request = requests.post(url, headers=headers)
