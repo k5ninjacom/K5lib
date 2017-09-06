6db1bccf84d09de8e2cc98938f2e1f91b02cd0db
mika <mika.hahtokari@fi.fujitsu.com>
2017-09-06 14:01:26 +0300

    add creation of sg in secure_me example

--- secure_me.py
+++ secure_me.py
@@ -17,13 +17,23 @@ r = requests.get(r'http://jsonip.com')
 ip = r.json()['ip']
 print ('Your IP is', ip)
 
+
+
+
 project_token = k5lib.get_project_token(user_name, password, domain, project_name, region)
 project_id = k5lib.get_project_id(user_name, password, domain, project_name, region)
+
+security_group_id = k5lib.get_security_group_id(project_token, region, 'sg_ext_mgmt')
+if security_group_id == 0:
+    k5lib.create_security_group(project_token, region, name='sg_ext_mgmt', description='external management')
+
+#print (security_group_id)
+
+
 
 security_groups = k5lib.list_security_groups(project_token, region)
 print (json.dumps(security_groups, indent=2))
 
-security_group_id = k5lib.get_security_group_id(project_token, region, 'sg_ext_mgmt')
-print (security_group_id)
+
 
 # rule_return_value = k5lib.create_security_group_rule(project_token, region, security_group_id, direction='ingress', )
