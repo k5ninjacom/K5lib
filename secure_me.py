1939390b1cb4cbd4f40f8208a4f67ed81b3d9b22
mika <mika.hahtokari@fi.fujitsu.com>
2017-09-07 09:58:04 +0300

    clean up secure_me example

--- secure_me.py
+++ secure_me.py
@@ -17,19 +17,16 @@ r = requests.get(r'http://jsonip.com')
 ip = r.json()['ip']
 print ('Your IP is', ip)
 
+# Get token for authentication
 project_token = k5lib.get_project_token(user_name, password, domain, project_name, region)
-project_id = k5lib.get_project_id(user_name, password, domain, project_name, region)
 
-# security_group_id = k5lib.get_security_group_id(project_token, region, 'sg_ext_mgmt')
-k5lib.create_security_group(project_token, region, name='sg_ext_mgmt', description='external management')
-
+# Get ID of security group we want to modify
 security_group_id = k5lib.get_security_group_id(project_token, region, 'sg_ext_mgmt')
 print (security_group_id)
 
-rule_return_value = k5lib.create_security_group_rule(project_token, region, security_group_id, direction='ingress', protocol='tcp', port_range_max=22, port_range_min=22, remote_ip_prefix=ip + '/32', remote_group_id=None)
-
+# Add ssh access rule with current IP address
+rule_return_value = k5lib.create_security_group_rule(project_token, region, security_group_id, direction='ingress',
+                                                     protocol='tcp', port_range_max=22, port_range_min=22,
+                                                     remote_ip_prefix=ip + '/32', remote_group_id=None)
 print (rule_return_value)
-
-security_group_list = k5lib.list_security_groups(project_token, region)
-print(json.dumps(security_group_list,indent=2))
 
