91defd407e9fd1535719c958f9cb04d1b5c2f4f8
mika <mika.hahtokari@fi.fujitsu.com>
2017-11-09 11:03:28 +0200

    add command line arguments into create_stack example

--- examples/create_stack.py
+++ examples/create_stack.py
@@ -2,6 +2,9 @@ from os import environ as env
 import sys
 sys.path.append('k5lib')
 import k5lib
+import argparse
+
+
 
 # Create a log file
 k5lib.create_logfile('create_stack.log')
@@ -11,12 +14,20 @@ password = env['OS_PASSWORD']
 domain = env['OS_USER_DOMAIN_NAME']
 projectName = env['OS_PROJECT_NAME']
 region = env['OS_REGION_NAME']
-templatefile = 'foobar.yml'
+
+# Setup command line parser
+parser = argparse.ArgumentParser(description="Get a password of server(s) ")
+parser.add_argument("templatefile", help="Template")
+parser.add_argument("stackname", help="Name of the stack")
+args = parser.parse_args()
+
+
+templatefile = args.templatefile
 
 projectToken = k5lib.get_project_token(username, password, domain, projectName, region)
 projectId = k5lib.get_project_id(username, password, domain, projectName, region)
 
-stackName = 'foobar'
+stackName = args.stackname
 
 with open(templatefile, 'r') as file:
     template = file.read()
