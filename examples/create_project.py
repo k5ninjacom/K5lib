bd5d8c420090343830911a8958d1c676ba777ab4
mika <mika.hahtokari@fi.fujitsu.com>
2017-12-19 10:57:07 +0200

    add command line arguments into create_project..py example

--- examples/create_project.py
+++ examples/create_project.py
@@ -1,7 +1,9 @@
 from os import environ as env
 import sys
+import argparse
 sys.path.append('k5lib')
 import k5lib
+
 
 username = env['OS_USERNAME']
 password = env['OS_PASSWORD']
@@ -13,8 +15,14 @@ projectName = 'foobar_project'
 # Create a log file
 k5lib.create_logfile('create_project.log')
 
+# Setup command line parser
+parser = argparse.ArgumentParser(description="Create project ")
+parser.add_argument("project_name", help="Name of the project")
+args = parser.parse_args()
+
+project_Name = args.project_name
 
 regionToken = k5lib.get_region_token(username, password, domain, region)
 domainId = k5lib.get_domain_id(username, password, domain)
-newproject = k5lib.create_project(regionToken, domainId, region, projectName)
+newproject = k5lib.create_project(regionToken, domainId, region, project_Name)
 print(newproject)
