8e20a370c720515d7fff69a8a2027dda6198e64c
mika <mika.hahtokari@fi.fujitsu.com>
2017-12-22 14:14:37 +0200

    add commandline parser into get_project_id.py in examples

--- examples/get_project_id.py
+++ examples/get_project_id.py
@@ -1,5 +1,6 @@
 from os import environ as env
 import sys
+import argparse
 sys.path.append('k5lib')
 import k5lib
 
@@ -12,5 +13,15 @@ domain = env['OS_USER_DOMAIN_NAME']
 projectName = env['OS_PROJECT_NAME']
 region = env['OS_REGION_NAME']
 
-projectId = k5lib.get_project_id(username, password, domain, projectName, region)
+# Setup command line parser
+parser = argparse.ArgumentParser(description="Get project ID ")
+parser.add_argument("project_name", help="Name of the project")
+args = parser.parse_args()
+
+project_Name = args.stackname
+
+
+
+
+projectId = k5lib.get_project_id(username, password, domain, project_Name, region)
 print(projectId)
