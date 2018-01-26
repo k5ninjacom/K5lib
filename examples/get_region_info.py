23a43e37b8f9bf29cd938150b4c68e89a5091b0b
mika <mika.hahtokari@fi.fujitsu.com>
2018-01-26 11:02:52 +0200

    add logging into get_region_info.py

--- examples/get_region_info.py
+++ examples/get_region_info.py
@@ -3,10 +3,11 @@ import sys
 sys.path.append('k5lib')
 import k5lib
 import json
+import logging
+
 
 # Create a log file
-k5lib.create_logfile('list_regions.log')
-
+k5lib.create_logfile('get_region_info.log')
 
 username = env['OS_USERNAME']
 password = env['OS_PASSWORD']
@@ -16,4 +17,5 @@ globalToken = k5lib.get_global_token(username, password, domain)
 
 region_info = k5lib.get_region_info(globalToken, 'fi-1')
 
+logging.info(json.dumps(region_info, indent=4))
 print(json.dumps(region_info, indent=4))
